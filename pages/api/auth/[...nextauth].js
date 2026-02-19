87f371cdb2981bd61cd7b7e155e07993cf7f3d17
cguy2025-creator <cguy2025@gmail.com>
2026-02-19 07:27:20 -0500

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -13,14 +13,12 @@ export default NextAuth({
     async jwt({ token, account }) {
       if (account) {
         token.accessToken = account.access_token;
-        token.refreshToken = account.refresh_token;
-        token.expiresAt = account.expires_at * 1000;
       }
       return token;
     },
     async session({ session, token }) {
-      session.accessToken = token.accessToken;
+      session.accessToken = token.accessToken; // This passes the token to the widget
       return session;
     },
   },
-});
+  secret: process.env.NEXTAUTH_SECRET, // This is mandatory for the session to "stick"
